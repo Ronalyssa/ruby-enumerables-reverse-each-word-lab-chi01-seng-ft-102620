6e693c4d7eb43ade622e44726b406159b38d284a
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-29 16:31:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,9 @@ require 'pry'
 def reverse_each_word(string)
   #reverse_array = []
   reverse_array = string.split
-  reverse_array.collect do |element|
-    element.reverse
-    element
+  reverse_array.collect do |string|
+    string.reverse
+    string
     binding.pry
   end
 reverse_array
